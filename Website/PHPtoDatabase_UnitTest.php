574e28f6439dfcbc05166cdbf18b91c3d1614070
Jacob Mink <minkjaco@tamu.edu>
2018-03-31 11:09:04 -0500

    PHP to Database unit Tests

--- Website/PHPtoDatabase_UnitTest.php
+++ Website/PHPtoDatabase_UnitTest.php
@@ -1,6 +1,22 @@
 <html>
 <head>
 <title>Database Unit Tests</title>
+<style>
+table {
+	font-family: arial, sans-serif;
+	border-collapse: collapse;
+	width: 100%;
+}
+td, th {
+	border: 1px solid #dddddd;
+	text-align: left;
+	padding: 8px;
+}
+
+tr:nth-child(even) {
+	background-color: #dddddd;
+}
+</style>
 </head>
 <body>
 
@@ -94,8 +110,6 @@ try {
 echo('$data: '.$data[0]['COUNT(*)']."<br>\n");
 echo("<br><br><br>\n");
 
-
-
 echo("Test 10: Check that total data in range gives expected result<br>\n");
 echo("Expect: \$data = 1<br>\n");
 try {
@@ -104,6 +118,77 @@ try {
 	echo($e->getMessage()."<br>\n");
 }
 echo('$data: '.$data."<br>\n");
+echo("<br><br><br>\n");
+
+echo("Test 11: Print data<br>\n");
+try {
+	$data = $db->Multi_PrintDataPoints(array("Time"), array(array("'2018-03-20 00:00:00'")), array(array("'2018-03-23 00:00:00'")));
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+echo("<br><br><br>\n");
+
+echo("Test 12: Get average in range<br>\n");
+try {
+	$data = $db->AverageInHourRange("Time", "2018-02-20 00:00:00", "2018-03-22 00:00:00", 13, 15);
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+echo("Data: $data<br>\n");
+echo("<br><br><br>\n");
+
+
+echo("Test 13: Get average in ranges<br>\n");
+try {
+	$data = $db->AverageInHourRanges("Time", "2018-02-20 00:00:00", "2018-03-22 00:00:00", array(13, 15, 17), array(15, 17, 19));
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+foreach ($data as $d) {
+	echo("Data: $d<br>\n");
+}
+echo("<br><br><br>\n");
+
+
+echo("Test 14: Get max in ranges<br>\n");
+try {
+	$data = $db->MaxInHourRanges("Time", "2018-02-20 00:00:00", "2018-03-22 00:00:00", array(13, 15, 17), array(15, 17, 19));
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+foreach ($data as $d) {
+	echo("Data: $d<br>\n");
+}
+echo("<br><br><br>\n");
+
+echo("Test 15: Get mode in ranges<br>\n");
+try {
+	$data = $db->MaxInHourRanges("Time", "2018-02-20 00:00:00", "2018-03-22 00:00:00", array(13, 15, 17), array(15, 17, 19));
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+foreach ($data as $d) {
+	echo("Data: $d<br>\n");
+}
+echo("<br><br><br>\n");
+
+echo("Test 16: Get median in ranges(even number of entries)<br>\n");
+try {
+	$data = $db->MedianInHourRange("Time", "2018-03-20 00:00:00", "2018-03-21 00:00:00", 13, 15);
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+echo("Data: $data<br>\n");
+echo("<br><br><br>\n");
+
+
+echo("Test 17: Get median in ranges(odd number of entries)<br>\n");
+try {
+	$data = $db->MedianInHourRange("Time", "2018-03-27 00:00:00", "2018-03-29 00:00:00", 15, 19);
+} catch (Exception $e) {
+	echo($e->getMessage()."<br>\n");
+}
+echo("Data: $data<br>\n");
 echo("<br><br><br>\n");
 
 ?>
